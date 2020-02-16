7d6723dadd41e27d7f68e39e951647d24b8bb11e
Tara Pun <pun.tara@outlook.com>
2020-02-16 12:20:14 -0800

    First Commit

---
+++ BubbleSort/BubbleSort/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] listNumbers = { 10, 15, 20, 2, 4, 7, 65};
+            PrintArray(listNumbers);
+            BobbleS(listNumbers);
+            PrintArray(listNumbers);
+
+        }
+        static void PrintArray(int[] arr)
+        {
+            foreach(int nums in arr)
+            {
+                Console.Write(nums + " ");
+            }
+            Console.WriteLine();
+        }
+        static void BobbleS(int[] arr)
+        {
+            for(int i =0; i < arr.Length - 1; i++)
+            {
+                for (int j = 0; j < arr.Length - 1 - i; j++) 
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+            
+        }
+    }
+}
